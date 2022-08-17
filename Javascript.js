c07d3f01f2abf83c1382f0a6033cf2b5568d8342
Mohan <66682632+RAVULAPALLIMOHAN@users.noreply.github.com>
2022-08-17 22:36:27 +0530

    onclick

--- Javascript.js
+++ Javascript.js
@@ -1,8 +1,16 @@
 function hideContent(){
     document.getElementById('content').innerHTML="";
 }
+function hideDemo(){
+    document.getElementById('demo').innerHTML ="";
+}
 function changecolor(){
     document.getElementById('content').style.color='red';
+}
+function hide(){
+    const cars = ["Saab","Volvo","BMW"];
+    const car = {name:"Audi",model:"A1"}
+document.getElementById("demo").innerHTML ='const <span class="black">cars = ["Saab","Volvo","BMW"];</span>'+'<br/>'+'const <span class="black">car = {name:"Audi",model:"A1"} </span>'+'<br/>'+'Result :car.model +","+ cars[2] ::   '+car.model +","+ cars[2];
 }
 
 
